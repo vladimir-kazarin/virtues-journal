94ba292ebeb78fbb811690d56fc65e24a8b3fdcd
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2019-01-18 04:36:51 +0000

    Adding new layout and signup page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,18 +15,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
   
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
